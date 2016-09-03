8a112e1ef8f252196aa1d0dbf4bd03695187afc1
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-09-03 14:29:16 +0200

    add character

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,4 +1,5 @@
 import RainbowText from 'objects/RainbowText';
+import Character from 'objects/Character';
 
 class GameState extends Phaser.State {
 
@@ -6,7 +7,19 @@ class GameState extends Phaser.State {
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
 		let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
 		text.anchor.set(0.5);
+
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    // Add the physics engine to all game objects
+    this.game.world.enableBody = true;
+
+    let hero = new Character(this.game, 20 , 20, "test", 0);
+    this.game.add.existing(hero);
 	}
+
+  preload() {
+    this.game.load.spritesheet('hero', 'res/blue_sheet.png', 32, 32);
+    this.game.load.image("test", "res/test.png", 32, 32);
+  }
 
 }
 
