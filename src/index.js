3a7cdc1961590a57043562243c501cfd41937cb7
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-09-03 16:33:43 +0200

    allow to go to new view

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import GameState from 'states/GameState';
+import ComputerScreen from 'states/ComputerScreen';
 
 class Game extends Phaser.Game {
 
-	constructor() {
-		super(500, 500, Phaser.AUTO, 'content', null);
-		this.state.add('GameState', GameState, false);
-		this.state.start('GameState');
-	}
+  constructor() {
+    super(500, 500, Phaser.AUTO, 'content', null);
+    this.state.add('GameState', GameState, false);
+    this.state.add('ComputerScreen', ComputerScreen, false);
+    this.state.start('GameState');
+  }
+
+  goToScreenMode() {
+    this.state.start('ComputerScreen');
+  }
 
 }
 
