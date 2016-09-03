0257168a3022bd7f540d26dba079adc205cd18a4
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-09-04 01:33:53 +0200

    add transition

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import GameState from 'states/GameState';
 import ComputerScreen from 'states/ComputerScreen';
 import TypeRecordScreen from 'states/TypeRecordScreen';
+import RoomToTypeRecorder from 'states/RoomToTypeRecorder';
 
 class Game extends Phaser.Game {
 
@@ -9,8 +10,9 @@ class Game extends Phaser.Game {
     this.state.add('GameState', GameState, false);
     this.state.add('ComputerScreen', ComputerScreen, false);
     this.state.add('TypeRecordScreen', TypeRecordScreen, false);
+    this.state.add('RoomToTypeRecorder', RoomToTypeRecorder, false);
     //this.state.start('GameState');
-    this.state.start('TypeRecordScreen');
+    this.state.start('GameState');
   }
 
   goToScreenMode() {
@@ -19,7 +21,11 @@ class Game extends Phaser.Game {
 
   goToMainGame() {
     const shouldReload = false;
-    this.state.start('GameState', true, false, shouldReload);
+    this.state.start('GameState', Phaser.Plugin.StateTransition.Out.SlideLeft, Phaser.Plugin.StateTransition.Out.SlideRight, true, false, shouldReload);
+  }
+
+  goToSecondLevel() {
+    this.state.start('RoomToTypeRecorder', Phaser.Plugin.StateTransition.Out.SlideRight, Phaser.Plugin.StateTransition.Out.SlideLeft);
   }
 
 
