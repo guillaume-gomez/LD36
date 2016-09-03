fe259d5b9dcc84d76415b4205906294938e53a54
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-09-03 19:59:11 +0200

    add tweens

--- src/states/TypeRecordScreen.js
+++ src/states/TypeRecordScreen.js
@@ -1,12 +1,12 @@
-const yPosition = 350;
+const yPosition = 370;
 
 const leftPosition = {
-  x: 182,
+  x: 203,
   y: yPosition
 }
 
 const rightPosition = {
-  x: 276,
+  x: 296,
   y: yPosition
 }
 
@@ -21,8 +21,13 @@ class TypeRecordScreen extends Phaser.State {
     const scaleRotator = 0.2;
     let rotatorLeft = this.game.add.sprite(leftPosition.x, leftPosition.y, "rotator");
     rotatorLeft.scale.setTo(scaleRotator, scaleRotator);
+    rotatorLeft.anchor.setTo(0.5, 0.5);
     let rotatorRight = this.game.add.sprite(rightPosition.x, rightPosition.y, "rotator");
+    rotatorRight.anchor.setTo(0.5, 0.5);
     rotatorRight.scale.setTo(scaleRotator, scaleRotator);
+
+    this.leftRotation = this.game.add.tween(rotatorLeft).to( { angle: 360 }, 2000, Phaser.Easing.Linear.None, true);
+    this.rightRotation = this.game.add.tween(rotatorRight).to( { angle: 360 }, 2000, Phaser.Easing.Linear.None, true);
   }
 
   preload() {
@@ -31,7 +36,8 @@ class TypeRecordScreen extends Phaser.State {
   }
 
   update() {
-    //
+    this.leftRotation.start();
+    this.rightRotation.start();
   }
 
 }
