d280f2138bec7efdb49fbfdeff11bb4c2983eaa8
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-09-03 15:53:15 +0200

    lock move if necessary

--- src/objects/Character.js
+++ src/objects/Character.js
@@ -7,6 +7,7 @@ class Character extends Phaser.Sprite {
     this.body.gravity.y = 600;
     this.body.bounce.x = this.body.bounce.y = 0;
     this.cursor = game.input.keyboard.createCursorKeys();
+    this.locked = false;
     //  const leftArray = [4, 5, 6, 7];
     //  const rightArray = [12, 13, 14, 15];
     //  const JumpArray = [1,2,3,4];
@@ -16,11 +17,12 @@ class Character extends Phaser.Sprite {
     // this.animations.add('right', rightArray, 10, true);
   }
 
-  onStart() {
-  }
-
-
   update() {
+    if(this.locked) {
+      this.body.velocity.x = 0;
+      return;
+    }
+
     if (this.cursor.left.isDown) {
         this.body.velocity.x = -200;
         //this.animations.play("left", 45, true);
@@ -38,6 +40,14 @@ class Character extends Phaser.Sprite {
     if (this.cursor.up.isDown && this.body.touching.down) {
       this.body.velocity.y = -250;
     }
+  }
+
+  lock() {
+    this.locked = true;
+  }
+
+  unlock() {
+    this.locked = false;
   }
 }
 
