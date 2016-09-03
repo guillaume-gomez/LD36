cae3acac9e37d58941a1123b65454f2c6e802c8b
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-09-03 14:45:37 +0200

    load tileSet and display it

---
+++ src/states/MainMenu.js
@@ -0,0 +1,19 @@
+import RainbowText from 'objects/RainbowText';
+
+
+class MainMenu extends Phaser.State {
+
+  create() {
+    //
+  }
+
+  preload() {
+  }
+
+  run() {
+    //
+  }
+
+}
+
+export default MainMenu;
