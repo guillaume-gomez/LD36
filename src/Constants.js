876d86686045e87d1c2a3368caab047ba11a2b2e
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-09-03 17:51:15 +0200

    init position next to computer

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,17 @@
 export const StringDialog = {
   onComputer: "Press enter to use the computer"
 }
+
+export const TextPosition = {
+  x: 100,
+  y: 20,
+  x0: 0,
+  y0: 400,
+  x1:200,
+  y1: 100
+};
+
+export const PositionAfterComputer = {
+  x: TextPosition.x1 + 80,
+  y: TextPosition.y0 + 20
+}
