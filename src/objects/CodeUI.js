fce982887e2256c7d687c2283d8479941bf27c40
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-09-04 12:57:47 +0200

    add callback function when the right password is entered

--- src/objects/CodeUI.js
+++ src/objects/CodeUI.js
@@ -1,26 +1,42 @@
 import { StringDialog, TextPosition , Style, Code } from '../Constants.js';
 class CodeUI extends Phaser.Text {
 
-  constructor(game, x) {
+  constructor(game, x, callBackFn) {
     super(game, x, TextPosition.y, "", Style);
     game.input.keyboard.addCallbacks(this, null, null, this.keyPress);
     this.setShadow(1, 1, 'rgba(0,0,0,0.7)', 1);
     this.setTextBounds(TextPosition.x0, TextPosition.y0, TextPosition.x1, TextPosition.y1);
+    this.hide = true;
+    this.callBack = callBackFn
   }
 
   keyPress(char) {
+    if(this.hide) {
+      return;
+    }
+
     if(char >= '0' && char <= '9') {
       this.text += char;
     }
     if(this.text === Code) {
-      //TODO
+      this.callBack();
+      this.hideText();
+      this.text = "";
+      return;
     }
     if(this.text.length >= Code.length) {
-      this.erase();
+      this.invalid();
     }
   }
 
-  erase() {
+  hideText() {
+    this.hide = true;
+  }
+  showText() {
+    this.hide = false;
+  }
+
+  invalid() {
     this.text = "Invalid";
     const fn = () => {this.text = ""};
     setTimeout(fn, 2000);
