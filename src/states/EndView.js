b123b6bdb1e9bc91b3658c80ac284fe6ed246715
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-10-04 09:51:11 +0200

    fix typo

--- src/states/EndView.js
+++ src/states/EndView.js
@@ -2,7 +2,7 @@ class EndView extends Phaser.State {
 
   create() {
     this.game.add.text(120, 200, "Thanks for playing  again ! :)", { font: "bold 21px Arial", fill: "#fff" })
-    this.game.add.text(40, 280, "I hope it was as much fun to play as to develop it!", { font: "bold 18px Arial", fill: "#fff" });
+    this.game.add.text(40, 280, "I hope you enjoyed playing it as much I did coding it!", { font: "bold 18px Arial", fill: "#fff" });
   }
 
   preload() {
